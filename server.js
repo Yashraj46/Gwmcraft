64f779a00f45ef6dae220e3a48a31a495a1875c9
Glitch (glitch-hello-node) <none>
2023-05-11 08:25:20 +0000

    🏠👂 Checkpoint ./server.js:14969446/147

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
-  host: '',
+  host: 'na1.node.sneakygames.net',
   username: process.env.user,
-  password: process.env.pass
+  password: process.env.pass, 
+  port: '30028', 
+  auth: 'microsoft'
 })
-
 bot.on('kicked', console.log)
 bot.on('error', console.log)
